b588a6fdc2c108303279ebed023a333ea9bbd800
Darasan <107739312+shanmugamtech@users.noreply.github.com>
2023-08-04 21:23:51 +0530

    portfolio.js

---
+++ Untitled-1.js
@@ -0,0 +1,78 @@
+function switchBgColor_white() {
+    let backgroundEl=document.getElementById("background");
+    backgroundEl.style.backgroundImage="radial-gradient(#fff,#1d3144)";
+    backgroundEl.style.height="50%";
+    document.getElementById("Im_color_change").style.color = "#000000";
+    document.getElementById("md_Im_color_change").style.color = "#000000";
+    document.getElementById("home_page_big_para_color_change").style.color = "#000000";
+    
+    
+
+    /*about Me page*/
+    let about_me_backgroundEl = document.getElementById("aboutMeBack");
+    about_me_backgroundEl.style.backgroundImage = "radial-gradient(#fff,#1d3144)";
+    about_me_backgroundEl.style.height = "50%"
+    document.getElementById("about_page_big_para_change").style.color = "#000";
+    document.getElementById("about_me_h1_change").style.color = "#000";
+    document.getElementById("md_about_me_h1_change").style.color = "#000";
+
+    /*Skill page*/
+    let skill_backgroundEl=document.getElementById("skill_background");
+    skill_backgroundEl.style.backgroundImage="radial-gradient(#fff,#1d3144)";
+    skill_backgroundEl.style.height="50%";
+
+    /*achievement page*/
+    let achievement_backgroundEl=document.getElementById("achievement_background");
+    achievement_backgroundEl.style.backgroundImage="radial-gradient(#fff,#1d3144)";
+    achievement_backgroundEl.style.height="50%";
+
+    /*Footer Page*/
+    let footer_backgroundEl=document.getElementById("footer_background");
+    footer_backgroundEl.style.backgroundImage="radial-gradient(#fff,#1d3144)";
+    footer_backgroundEl.style.height="50%";
+    document.getElementById("footer_address_para_change").style.color = "#000";
+    document.getElementById("footer_home_change").style.color = "#000";
+    document.getElementById("footer_about_me_change").style.color = "#000";
+    document.getElementById("footer_skills_change").style.color = "#000";
+    document.getElementById("footer_achievement_change").style.color = "#000";
+
+}
+
+function switchBgColor_nrml() {
+    let backgroundEl=document.getElementById("background");
+    backgroundEl.style.backgroundImage="radial-gradient(#277BC0,#1d3144)";
+    backgroundEl.style.height="50%";
+    document.getElementById("Im_color_change").style.color = "#fff";
+    document.getElementById("md_Im_color_change").style.color = "#fff";
+    document.getElementById("home_page_big_para_color_change").style.color = "#fff";
+    
+    
+
+    /*about Me page*/
+    let about_me_backgroundEl = document.getElementById("aboutMeBack");
+    about_me_backgroundEl.style.backgroundImage = "radial-gradient(#277BC0,#1d3144)";
+    about_me_backgroundEl.style.height = "50%"
+    document.getElementById("about_page_big_para_change").style.color = "#fff";
+    document.getElementById("about_me_h1_change").style.color = "#fff";
+    document.getElementById("md_about_me_h1_change").style.color = "#fff";
+
+    /*Skill page*/
+    let skill_backgroundEl=document.getElementById("skill_background");
+    skill_backgroundEl.style.backgroundImage="radial-gradient(#277BC0,#1d3144)";
+    skill_backgroundEl.style.height="50%";
+
+    /*achievement page*/
+    let achievement_backgroundEl=document.getElementById("achievement_background");
+    achievement_backgroundEl.style.backgroundImage="radial-gradient(#277BC0,#1d3144)";
+    achievement_backgroundEl.style.height="50%";
+
+    /*Footer Page*/
+    let footer_backgroundEl=document.getElementById("footer_background");
+    footer_backgroundEl.style.backgroundImage="radial-gradient(#277BC0,#1d3144)";
+    footer_backgroundEl.style.height="50%";
+    document.getElementById("footer_address_para_change").style.color = "#fff";
+    document.getElementById("footer_home_change").style.color = "#fff";
+    document.getElementById("footer_about_me_change").style.color = "#fff";
+    document.getElementById("footer_skills_change").style.color = "#fff";
+    document.getElementById("footer_achievement_change").style.color = "#fff";
+}
