dfba87b9ea656af7b7aefecf1ea24e7f841d2b54
shanmugamtech <shanmugamdarasan@gmail.com>
2024-08-02 14:55:05 +0530

    Updateing all the files

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // toggle button element
 const toggleButton = document.querySelector('.toggle_button');
 
@@ -13,19 +12,3 @@ if (toggleButton && navElement) {
         navElement.classList.toggle('active');
     });
 }
-=======
-// toggle button element
-const toggleButton = document.querySelector('.toggle_button');
-
-// navigation element
-const navElement = document.querySelector('.navbar_elem');
-
-// Check if toggleButton and navElement are not undefined
-if (toggleButton && navElement) {
-    // Add click event listener to the toggle button
-    toggleButton.addEventListener('click', () => {
-        // Toggle the 'active' class for the navigation element
-        navElement.classList.toggle('active');
-    });
-}
->>>>>>> origin/main
